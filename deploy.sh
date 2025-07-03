74be5097cb568d1e15592361dd266fff1fcf0414
Ankit Tayal <tayalankit20@gmail.com>
2025-07-04 02:45:08 +0530

    add OneNote pdf

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push tayalankit20/multi-client:$SHA
 docker push tayalankit20/multi-server:$SHA
 docker push tayalankit20/multi-worker:$SHA
 
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=tayalankit20/multi-server:$SHA
-kubectl set image deployments/client-deployment client=tayalankit20/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=tayalankit20/multi-worker:$SHA
+# kubectl apply -f k8s
+# kubectl set image deployments/server-deployment server=tayalankit20/multi-server:$SHA
+# kubectl set image deployments/client-deployment client=tayalankit20/multi-client:$SHA
+# kubectl set image deployments/worker-deployment worker=tayalankit20/multi-worker:$SHA
